0c90e966dff9210bb8f7cc7909b539c9144dd38f
jasoncarcamo <47464765+jasoncarcamo@users.noreply.github.com>
2025-03-17 21:34:26 -0400

    Ready for prod testing

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-
-
 module.exports = {
-    url: process.env.NODE_ENV === "development" ? process.env.REACT_APP_TEST_URL : process.env.REACT_APP_PROD_URL 
+    url: process.env.REACT_APP_NODE_ENV === "development" ? process.env.REACT_APP_TEST_URL : process.env.REACT_APP_PROD_URL 
 };
